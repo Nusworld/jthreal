05323401539c4a59e1eee612ddd3b5ae27d5200c
Nusworld <liuzehao3@outlook.com>
2021-12-10 09:53:35 +0800

    运行时抛出异常

---
+++ src/cn/edu/ncu/erlys/liu/stopthread/RunThrowException.java
@@ -0,0 +1,11 @@
+package cn.edu.ncu.erlys.liu.stopthread;
+
+public class RunThrowException {
+    public void aVoid() throws Exception {
+        throw new Exception();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
