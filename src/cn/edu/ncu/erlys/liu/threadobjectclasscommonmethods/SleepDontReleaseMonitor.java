048678089dd6d9d59bdb1899c46208dde3d66bc1
Nusworld <liuzehao3@outlook.com>
2021-12-10 09:59:40 +0800

    sleep 并不释放已经获取的锁

---
+++ src/cn/edu/ncu/erlys/liu/threadobjectclasscommonmethods/SleepDontReleaseMonitor.java
@@ -0,0 +1,30 @@
+package cn.edu.ncu.erlys.liu.threadobjectclasscommonmethods;
+
+import javax.management.monitor.Monitor;
+
+/**
+ * 展示线程sleep时不释放synchronized的monitor锁
+ */
+public class SleepDontReleaseMonitor implements Runnable {
+
+    public static void main(String[] args) {
+        SleepDontReleaseMonitor sleepDontReleaseMonitor = new SleepDontReleaseMonitor();
+        new Thread(sleepDontReleaseMonitor).start();
+        new Thread(sleepDontReleaseMonitor).start();
+    }
+
+    @Override
+    public void run() {
+        syn();
+    }
+
+    private synchronized void syn() {
+        System.out.println("线程" + Thread.currentThread().getName() + "获取到了Monitor");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("线程" + Thread.currentThread().getName() + "退出了同步代码块");
+    }
+}
